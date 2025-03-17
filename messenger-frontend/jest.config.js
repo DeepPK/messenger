f13a5fe1cefa2ff94c8f5056a1b34bf74986099b
Pavel <DeepKzl@yandex.ru>
2025-03-17 16:51:37 +0300

    test

--- messenger-frontend/jest.config.js
+++ messenger-frontend/jest.config.js
@@ -1,15 +1,20 @@
 module.exports = {
   reporters: [
     'default',
-    ['jest-junit', {
-      outputDirectory: 'test-report',
-      outputName: 'junit-test-results.xml',
-      suiteNameTemplate: '{filepath}',
-      ancestorSeparator: ' > '
-    }]
+    [
+      'jest-junit',
+      {
+        outputDirectory: 'test-report',
+        outputName: 'junit-test-results.xml',
+        suiteName: 'Jest Tests'
+      }
+    ]
   ],
   collectCoverage: true,
   coverageReporters: ['lcov', 'text'],
   coverageDirectory: 'coverage',
-  testEnvironment: 'jsdom'
+  testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1'
+  }
 };
